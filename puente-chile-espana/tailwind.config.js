fced6f4c1af73482b9c63a763e11f4213569a1b7
Sergio-HMC <contacto@homemarketconsulting.com>
2025-09-15 19:48:47 +0200

    Update tailwind.config.js

--- puente-chile-espana/tailwind.config.js
+++ puente-chile-espana/tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          primary: "#204975",     // azul principal
+          prussian: "#142E4A",    // azul prusiano
+          steel: "#8D99AE",       // gris azulado
+          cornsilk: "#FAF4D3",    // amarillo claro
+          tea: "#C2EFB3"          // verde claro
+        }
+      },
+      fontFamily: {
+        sans: ["Lato", "ui-sans-serif", "system-ui", "Arial", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 };
